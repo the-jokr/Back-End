e0749e6ab03b933727d1cad0f507756b49442a65
wisdomtooth <cbtsao47@gmail.com>
2019-05-20 12:11:15 -0500

    setup tables roles users jokes joke-wallet

--- data/migrations/20190519231327_initial.js
+++ data/migrations/20190519231327_initial.js
@@ -8,9 +8,40 @@ exports.up = function(knex, Promise) {
       field.increments();
       field.string("username", 50);
       field.string("password", 50);
+      field
+        .integer("roles_id")
+        .references("id")
+        .inTable("roles")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("jokes", field => {
+      field.increments();
+      field.string("category", 50);
+      field.text("question", 500);
+      field.text("punch_line", 500);
+    })
+    .createTable("joke_wallet", field => {
+      field.increments();
+      field
+        .integer("user_id")
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      field
+        .integer("joke_id")
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
     });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("users").dropTableIfExists("roles");
+  return knex.schema
+    .dropTableIfExists("joke_wallet")
+    .dropTableIfExists("jokes")
+    .dropTableIfExists("users")
+    .dropTableIfExists("roles");
 };

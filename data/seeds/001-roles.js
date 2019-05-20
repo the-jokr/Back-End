65dff3af4172c40549289beb2c6312db78bbaa19
wisdomtooth <cbtsao47@gmail.com>
2019-05-20 15:19:10 -0500

    add pg support

--- data/seeds/001-roles.js
+++ data/seeds/001-roles.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("roles")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("roles").insert([{ name: "Admin" }, { name: "Regular" }]);

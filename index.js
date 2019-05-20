f971f4b3d58b3e21811892b4a441c46dd6f9a172
wisdomtooth <cbtsao47@gmail.com>
2019-05-20 16:30:50 -0500

    set up base msg at /

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const server = require("./api/server");
 const port = process.env.PORT || 5000;
+server.get("/", (req, res) => {
+  res.send("server is live!🔥");
+});
 server.listen(port, () => {
   console.log(`Server is live at ${port} 🔥`);
 });

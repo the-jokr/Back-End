1802b59e3ce51013cec802359f63d29f2db6727b
wisdomtooth <cbtsao47@gmail.com>
2019-05-20 15:21:13 -0500

    fix truncate

--- data/seeds/001-roles.js
+++ data/seeds/001-roles.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("roles")
-    .del()
+    .truncate()
     .then(function() {
       // Inserts seed entries
       return knex("roles").insert([{ name: "Admin" }, { name: "Regular" }]);

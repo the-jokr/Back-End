17dd8b49d528943cfb60bf79d07fc1d4c2947eab
wisdomtooth <cbtsao47@gmail.com>
2019-05-20 23:11:18 -0500

    add Auth0

--- api/server.js
+++ api/server.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const server = express();
 const serverConfig = require("./serverConfig.js");
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-cbtsao.auth0.com/.well-known/jwks.json"
+  }),
+  audience: "https://jokr/api/",
+  issuer: "https://dev-cbtsao.auth0.com/",
+  algorithms: ["RS256"]
+});
+
+//routes
 const authRoute = require("./routes/usersRoute");
 const jokesRoute = require("./routes/jokesRoute");
 const walletRoute = require("./routes/walletRoute");
+
+//middleware
 const { errorHandler } = require("./middleware/errorHandler");
 const { protected } = require("./middleware/auth");
 serverConfig(server);
 
 server.use("/api/auth", authRoute);
 server.use("/api/jokes", jokesRoute);
-server.use("/api/wallet", protected, walletRoute);
+server.use("/api/wallet", jwtCheck, walletRoute);
 
 server.use(errorHandler);
 
